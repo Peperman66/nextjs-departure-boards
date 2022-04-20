1650e09d5c7b882d435cb95e0c2ba9be5394eca0
Peperman66 <peperman66@seznam.cz>
2022-04-21 00:32:58 +0200

    add dbHandler

---
+++ handlers/dbHandler.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client"
+import { Station } from "../types/station"
+import { Timetable } from "../types/timetable"
+import { Train } from "../types/train"
+
+const prisma = new PrismaClient()
+
+export const getStations = async () => {
+	const stations = await prisma.station.findMany() as Station[]
+	return stations
+}
+
+export const createStation = (station: Station) => {
+	return prisma.station.create({
+		data: {
+			id: station.id,
+			name: station.name
+		}
+	})
+}
+
+export const getTrains = async () => {
+	const trains = await prisma.train.findMany({
+		include: {
+			departures: {
+				orderBy: {
+					departure: 'asc',
+					arrival: 'asc'
+				}
+			}
+		}
+	}) as (Train & {departures: Timetable[]})[];
+	return trains;
+}
